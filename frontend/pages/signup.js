bc78b1713e37b322aaabe1f5bfdfc75d51295506
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-08-17 21:13:56 -0700

    signup page pt1

---
+++ frontend/pages/signup.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import styles from "../styles/SignUp.module.css";
+
+export default function SignUp() {
+  return (
+    <Layout>
+      <div className={styles.signupScreen}>
+        <h1>Create An Account</h1>
+      </div>
+    </Layout>
+  );
+}
