1e85bb5066f381e8753c5ff246beb9267242b97e
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-08-26 19:49:55 -0700

    designing login page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
+import Link from "next/link";
 import Layout from "../components/Layout";
+import styles from "../styles/SignUp.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitAccount = () => {};
+
   return (
     <Layout>
-      <h1>Login Page</h1>
+      <div className={styles.signupScreen}>
+        <h1>Sign In</h1>
+
+        <form onSubmit={handleSubmitAccount}>
+          <div className={styles.inputs}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Enter an Email"
+              onChange={(e) => setEmail(e.target.value)}
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.inputs}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              value={password}
+              placeholder="Enter Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className={styles.input}
+            />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <div className={styles.login_link}>
+          <p>
+            Don't have an account?{" "}
+            <Link href="/login">
+              <a>Sign Up Here</a>
+            </Link>
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 }
